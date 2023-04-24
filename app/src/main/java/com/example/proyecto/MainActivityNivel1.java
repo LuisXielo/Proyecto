9a932859e0025449e10217ea5b3a9bd026ff2108
LuisXielo <luismi1991.lmmg@gmail.com>
2023-04-24 10:00:46 +0200

    pruebas nivel 1

--- app/src/main/java/com/example/proyecto/MainActivityNivel1.java
+++ app/src/main/java/com/example/proyecto/MainActivityNivel1.java
@@ -2,22 +2,75 @@ package com.example.proyecto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivityNivel1 extends AppCompatActivity {
 
-    private TextView tv1, tv2, tv3;
+    private TextView tv_nombre, tv_score;
+    private ImageView iv_Auno, iv_Ados, iv_vidas;
+    private EditText et_respuesta;
+    private MediaPlayer mp, mp_great, mp_bad;
+
+    int score, numAleatorio_uno, numAleatorio_dos, resultado, vidas = 3;
+    String nombre_jugador, string_score, string_vidas;
+
+    String numero [] = {"cero","uno","dos","tres","cuatro","cinco","seis","siete","ocho","nueve"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_nivel1);
 
-        tv1 = findViewById(R.id.txt_usuario);
+        tv_nombre = findViewById(R.id.txt_usuario);
+        tv_score = findViewById(R.id.txt_puntuacion);
+        et_respuesta = findViewById(R.id.et_numero);
 
         String texto = getIntent().getStringExtra("texto");
-        tv1.setText(texto);
+        tv_nombre.setText(texto);
 
+    }
+
+    public void NumAleatorio(){
+        if(score <= 9){
+
+            numAleatorio_uno = (int) (Math.random() * 10);
+            numAleatorio_dos = (int) (Math.random() * 10);
+
+            resultado = numAleatorio_uno + numAleatorio_dos;
+
+            if(resultado <= 10){
+
+                for (int i = 0; i < numero.length; i++){
+                    int id = getResources().getIdentifier(numero[i], "drawable", getPackageName());
+                    if(numAleatorio_uno == i){
+                        iv_Auno.setImageResource(id);
+                    }if(numAleatorio_dos == i){
+                        iv_Ados.setImageResource(id);
+                    }
+                }
+
+            } else {
+                NumAleatorio();
+            }
+
+        }else {
+            Intent intent = new Intent(this, MainActivityNivel2.class);
+
+            string_score = String.valueOf(score);
+            string_vidas = String.valueOf(vidas);
+            intent.putExtra("jugador", nombre_jugador);
+            intent.putExtra("score", string_score);
+            intent.putExtra("vidas", string_vidas);
+
+            startActivity(intent);
+            finish();
+            mp.stop();
+            mp.release();
+        }
     }
 }
